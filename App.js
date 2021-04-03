96b19f97699a927d4019a362043c45cc12f0c854
ysaiascampos <ysaiascampos@gmail.com>
2021-04-03 20:24:48 -0300

    Button

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Dimensions } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Dimensions, TouchableHighlight } from 'react-native';
 
 //const {width, height } = Dimensions.get('window');
 export default function App() {
+  const [text, setText] = useState('');
+  const [sibmit, setSibmit] = useState('');
   return (
     <View style={styles.container}>
-      <Text>hola mundo!</Text>
-      <TextInput style={styles.input} placeholder='Escribe aqui' />
+      <Text>Text: {sibmit}</Text>
+      <TextInput 
+        style={styles.input} 
+        placeholder='Escribe aqui'
+        onChangeText={t => setText(t)}
+        defaultValue={text}
+      />
+      <TouchableHighlight
+        underlayColor={'#999'}
+        onPress={() => {
+          setSibmit(text)
+          alert('texto enviado con exito')
+        }}
+      ><Text>Aceptar</Text></TouchableHighlight>
       <StatusBar />
     </View>
   );
@@ -17,7 +31,8 @@ const styles = StyleSheet.create({
   input: {
     height: 40,
     width: '100%',
-    backgroundColor: '#eee',
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
   },
   container: {
     flex: 1,
