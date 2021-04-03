390b81b72114ab05749201b22ba9fb145ce7249b
ysaiascampos <ysaiascampos@gmail.com>
2021-04-02 22:21:06 -0300

    imput Dimensions

--- App.js
+++ App.js
@@ -1,46 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Dimensions } from 'react-native';
 
-const Texto = ({ style }) => {
-  const [texto, setTexto] = useState("Hola mundo");
-  const actualizaTexto = () => {
-    setTexto('Chao mundo!');
-  }
-  return (
-    <Text style={[styles.text, style]} onPress={actualizaTexto}>{texto}</Text>
-  )
-}
+//const {width, height } = Dimensions.get('window');
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto style={styles.red} />
-      <Texto style={styles.green} />
-      <Texto style={styles.blue} />
-      <StatusBar style="auto" />
+      <Text>hola mundo!</Text>
+      <TextInput style={styles.input} placeholder='Escribe aqui' />
+      <StatusBar />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  text: {
-    color: 'white',
-    fontSize: 24,
-  },
-  red: {
-    backgroundColor: 'red',
-  },
-  green: {
-    backgroundColor: 'green',
-  },
-  blue: {
-    backgroundColor: 'blue',
+  input: {
+    height: 40,
+    width: '100%',
+    backgroundColor: '#eee',
   },
   container: {
     flex: 1,
-    flexDirection: 'column',
     backgroundColor: '#fff',
-    alignItems: 'baseline',
-    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
