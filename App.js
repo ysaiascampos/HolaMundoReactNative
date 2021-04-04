3513f0e389c65945aec1f49e9ee3efcab386f4fc
ysaiascampos <ysaiascampos@gmail.com>
2021-04-04 19:04:25 -0300

    Modal

--- App.js
+++ App.js
@@ -1,34 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, ImageBackground,Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Modal,Text, View, Button } from 'react-native';
 
 export default function App() {
-  
+  const [modal, setModal] = useState(false);
   return (
     <View style={styles.container}>
-      <ImageBackground
-        style={styles.photo}
-        source={{
-          uri: 'https://placekitten.com/200/200'
-        }}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modal}
       >
-        <Text>hola sss</Text>
-      </ImageBackground>
+        <View style={styles.center}>
+          <View style={styles.content}>
+            <Text>Soy un modal</Text>
+            <Button title="Cerrar modal" onPress={() => setModal(!modal)} />
+          </View>
+        </View>
+      </Modal>
+      <Text>NO soy un modal</Text>
+      <Text>NO soy un modal</Text>
+      <Text>NO soy un modal</Text>
+      <Text>NO soy un modal</Text>
+
+      <Text>NO soy un modal</Text>
+      <Text>NO soy un modal</Text>
+      <Text>NO soy un modal</Text>
+      <Text>NO soy un modal</Text>
+      <Button title="Abrir modal" onPress={() => setModal(!modal)} />
       
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  photo: {
-    height: 200,
-    width: 200,
+  content: {
+    backgroundColor: '#eee',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 25,
+  },
+  center: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.3)'
   },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'stretch',
+    alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: 22,
   },
 });
