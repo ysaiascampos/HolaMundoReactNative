3d40a269415b11da7bc0c409c2ebee4e24274676
ysaiascampos <ysaiascampos@gmail.com>
2021-04-03 21:59:37 -0300

    ActivityIndicator

--- App.js
+++ App.js
@@ -1,30 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React from 'react';
+import { StyleSheet, ActivityIndicator, View } from 'react-native';
 
 export default function App() {
-  const [users, setUsers] = useState([])
-  const [loeading, setLoeading] = useState(true)
-
-  useEffect(() => {
-    fetch('http://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => {
-        setUsers(data)
-        setLoeading(false)
-      })
-  }, [])
-
-  if(loeading){
-    return <View style={styles.center}><Text>Cargando...</Text></View>
-  }
+  
   return (
     <View style={styles.container}>
-      <FlatList
-        data={users}
-        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
-        keyExtractor={item => String(item.id)}
+      <ActivityIndicator 
+        size="small"
+        color="#0000FF"
       />
+      
     </View>
   );
 }
@@ -37,17 +23,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingTop: 22,
   },
-  item: {
-    padding: 10,
-    fontSize: 22,
-    height: 50,
-    borderBottomColor: '#ccc',
-    borderBottomWidth: 1,
-
-  },
-  center: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
 });
