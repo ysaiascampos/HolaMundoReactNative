e31d584d8525324fe57db2993dd51595680adfa5
ysaiascampos <ysaiascampos@gmail.com>
2021-04-04 19:13:13 -0300

    Alert

--- App.js
+++ App.js
@@ -1,52 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Modal,Text, View, Button } from 'react-native';
-
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+const crearDialogo = () => Alert.alert('Titulo', 
+'Subtitulo o mensaje que podemos agregar',
+[
+  {
+    text: 'Cancelar',
+    onPress: () => {},
+    style: "cancel",
+  },
+  {
+    text: 'Aceptar',
+    onPress: () => console.log('Boton presionado'),
+  },
+  {
+    cancelable: false,
+  }
+]
+)
 export default function App() {
   const [modal, setModal] = useState(false);
   return (
     <View style={styles.container}>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modal}
-      >
-        <View style={styles.center}>
-          <View style={styles.content}>
-            <Text>Soy un modal</Text>
-            <Button title="Cerrar modal" onPress={() => setModal(!modal)} />
-          </View>
-        </View>
-      </Modal>
-      <Text>NO soy un modal</Text>
-      <Text>NO soy un modal</Text>
-      <Text>NO soy un modal</Text>
-      <Text>NO soy un modal</Text>
-
-      <Text>NO soy un modal</Text>
-      <Text>NO soy un modal</Text>
-      <Text>NO soy un modal</Text>
-      <Text>NO soy un modal</Text>
-      <Button title="Abrir modal" onPress={() => setModal(!modal)} />
-      
+      <Button title="Abrir dialogo" onPress={crearDialogo} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  content: {
-    backgroundColor: '#eee',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: 25,
-  },
-  center: {
-    flex: 1,
-    alignItems: 'stretch',
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0,0,0,0.3)'
-  },
   container: {
     flex: 1,
     alignItems: 'center',
