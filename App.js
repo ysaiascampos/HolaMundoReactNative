5f1b71f93188a6c7e8da9aba91820bf58823a094
ysaiascampos <ysaiascampos@gmail.com>
2021-04-04 21:30:45 -0300

    Location

--- App.js
+++ App.js
@@ -1,28 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { Alert, Button, StyleSheet, Text, View } from 'react-native';
-const crearDialogo = () => Alert.alert('Titulo', 
-'Subtitulo o mensaje que podemos agregar',
-[
-  {
-    text: 'Cancelar',
-    onPress: () => {},
-    style: "cancel",
-  },
-  {
-    text: 'Aceptar',
-    onPress: () => console.log('Boton presionado'),
-  },
-  {
-    cancelable: false,
-  }
-]
-)
+import React, { useEffect } from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import Constants from 'expo-constants';
+
+
 export default function App() {
-  const [modal, setModal] = useState(false);
+  const buscaLocation = async () => {
+    const { status } = await Location.requestPermissionsAsync();
+    if(status !== 'granted') {
+      return Alert.alert('No tenemos los permisos necesarios para acceder a la location');
+    }
+
+    const location = await Location.getCurrentPositionAsync({accuracy: 6})
+    console.log(location);
+  }
+  useEffect(() => {
+    buscaLocation()
+  })
   return (
     <View style={styles.container}>
-      <Button title="Abrir dialogo" onPress={crearDialogo} />
+      <Text>hola</Text>
     </View>
   );
 }
